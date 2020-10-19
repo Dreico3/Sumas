cd6d0ac8e8c078a9c2f5e9198f953e606a3ce625
Erick Cruz <fis9.81@gmail.com>
2020-10-19 18:49:35 -0400

    Funciona la suma multiple

---
+++ practicaconsola/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace practicaconsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b="";
+            string aux="";
+            string a = "20+60+50";
+            string operador = "";
+            int numero = 0;
+            int cont=0;
+           
+            Console.WriteLine("-------------------");
+            foreach (var num in a)
+            {
+                aux = Convert.ToString(num);
+
+                if (aux.Equals("+"))
+                {
+                    
+                    numero += int.Parse(b);
+                   
+                    b = "";
+                    cont++;
+                    operador = "+";
+
+                }
+                else if (aux.Equals("-"))
+                {
+                    
+                    numero -= int.Parse(b);
+                    
+                    b = "";
+                    cont++;
+                    operador = "-";
+                }
+                else if (aux.Equals("*")) { }
+
+                else
+                {
+                    b = b + aux;
+                }
+            }
+            if (!b.Equals(""))
+            {
+                if (operador.Equals("+"))
+                {
+                    numero += int.Parse(b);
+                    b = "";
+                    Console.WriteLine("b es distiento de 0");
+
+                }else if (operador.Equals("-"))
+                {
+                    numero -= int.Parse(b);
+                    b = "";
+                    Console.WriteLine("b es distiento de 0");
+
+                }
+            }
+            Console.WriteLine(b + "Resulatado--->>" + numero + " cont-->" + cont);
+        }
+    }
+}
